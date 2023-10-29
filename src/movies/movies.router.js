4ab4f209ef8d6f656eb44fd8dc2cd1c9a16708b7
stmiok <128644466+stmiok@users.noreply.github.com>
2023-10-28 19:49:38 -0700

    added missing ")" and fixed typo in movies.router

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -3,7 +3,7 @@ const controller = require("./movies.controller");
 
 router.route("/").get(controller.list);
 
-router.route("/:movieId([0-9]+/reviews").get(controller.listMoviesReviews);
+router.route("/:movieId([0-9]+)/reviews").get(controller.listMovieReviews);
 
 router.route("/:movieId([0-9]+)/theaters").get(controller.listTheaters);
 
