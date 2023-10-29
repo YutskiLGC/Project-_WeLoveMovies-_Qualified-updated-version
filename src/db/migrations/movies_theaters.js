8d5a8e16e6ee950b472267f27b400c457173576f
stmiok <128644466+stmiok@users.noreply.github.com>
2023-10-29 13:06:40 -0700

    deleted some code that was not needed

--- src/db/migrations/movies_theaters.js
+++ src/db/migrations/movies_theaters.js
@@ -1,6 +1,5 @@
 exports.up = function (knex) {
   return knex.schema.createTable("movies_theaters", (table) => {
-    // table.increments("id").primary().unsigned();
     table.integer("movie_id").unsigned();
     table.integer("theater_id").unsigned();
     table.boolean("is_showing").defaultTo(false);
